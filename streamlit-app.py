8261ece469ca730402735663a626ecb08520e399
Rubens Kina <rhkina@gmail.com>
2023-06-07 17:52:39 -0300

    Create styled form

--- streamlit-app.py
+++ streamlit-app.py
@@ -15,10 +15,31 @@ with open("style.css") as f:
 logo = Image.open('favicon.png')
 st.image(logo, width=150)
 
-st.title("BgCut - Background Cut")
-st.subheader("Remova o fundo das imagens com uso de Inteligência Artificial!")
-st.subheader("Arraste e solte os arquivos das imagens no local reservado no painel ao lado e veja o resultado!")
-st.subheader("Para fazer download para o seu computador basta clicar na imagem com o botão direito e selecionar 'Salvar imagem como...' ")
+st.title("BgCut - Background Cut using AI")
+st.subheader("Remove background from images using AI!")
+st.subheader("Just drag and drop image files and see the result! Multiple files allowed!")
+st.subheader("Right click the resulting images and select 'Save as...' to download to your computer")
+
+st.subheader(":mailbox: Hi! Do you want to receive our news occasionally? No spams, we hate spams as you do!")
+
+contact_form = """
+    <form action="https://formsubmit.co/rhkina@gmail.com" method="POST">
+        <input type="hidden" name="_captha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here"></textarea>
+        <button type="submit">Send</button>
+    </form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use local CSS file
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style/style.css")
 
 col1, col2 = st.columns(2)
 images = st.sidebar.file_uploader("Carregar imagens", accept_multiple_files =True)
@@ -26,11 +47,11 @@ if images:
     for image in images:
         with Image.open(image) as img:
             with col1:
-                st.header("Antes")
+                st.header("Before")
                 st.image(img)
 
             with col2:
                 output = remove(img)
-                st.header("Depois")
+                st.header("After")
                 processed_img = st.image(output)
 
